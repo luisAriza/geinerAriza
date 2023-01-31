fe37dd9ff6a8eff7d00858f425502b1660c672c6
Luis Ariza <luismanuelariza@hotmail.com>
2023-01-31 11:42:31 -0500

    Estilos cargados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#FFDD33",
+				secondary: "#191D34",
+				seconLight: "#212645",
+				seconDark: "#111322",
+				seconLight50: "rgba(58, 65, 120, 0.5)",
+			},
+			backgroundColor: (theme) => ({
+				...theme("colors"),
+				primary: "#FFDD33",
+				secondary: "#191D34",
+				seconLight: "#212645",
+				seconDark: "#111322",
+				seconLight50: "rgba(58, 65, 120, 0.5)",
+			}),
+			fontFamily: {
+				title: ["IstokWeb", "sans-serif"],
+				body: ["OpenSans", "sans-serif"],
+			},
+			dropShadow: {
+				card: "0 4px 8px #111322",
+				title: "0 4px 4px #111322",
+			},
+		},
 	},
 	plugins: [],
 };
